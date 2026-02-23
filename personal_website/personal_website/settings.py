e7cbecaa2d7a9b2f7386deae13cb9d46661d4709
hunterdhawkins <hunterdhawkins@gmail.com>
2026-02-23 08:54:07 -0800

    Working on transfering to dotenv for production

--- personal_website/personal_website/settings.py
+++ personal_website/personal_website/settings.py
@@ -9,28 +9,26 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
-from pathlib import Path
 import os
+from dotenv import load_dotenv
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cu21_@_0=%*o8e$gjuy&4$2)(^36wic4ihb&r&ae=z^hg3qlgd'
-
+SECRET_KEY = os.getenv('DJANGO_DEBUG')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
